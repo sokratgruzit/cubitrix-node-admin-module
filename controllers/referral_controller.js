30bad44b1cc2935b4f14cbb67ebfce10294c409f
gogaokradze <deathofsuper@gmail.com>
2023-07-21 17:02:34 +0400

    fix

--- controllers/referral_controller.js
+++ controllers/referral_controller.js
@@ -49,8 +49,69 @@ const delete_referral_settings = async (req, res) => {
   }
 };
 
+async function testunicalc() {
+  let daysBetween = getdaysBetween();
+  let uni_days = "daily";
+
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+  const currentDayOfWeek = currentDate.getDay();
+
+  if (uni_days == "daily") {
+    await referral_controller.uni_comission_count(1);
+  } else if ((uni_days = "montly")) {
+    if (currentDay === 1) {
+      await referral_controller.uni_comission_count(daysBetween);
+    }
+  } else if ((uni_days = "weekly")) {
+    if (currentDayOfWeek === 1) {
+      await referral_controller.uni_comission_count(7);
+    }
+  }
+}
+
+async function testbinarycalc() {
+  let daysBetween = getdaysBetween();
+  let uni_days = "daily";
+
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+  const currentDayOfWeek = currentDate.getDay();
+
+  if (uni_days == "daily") {
+    await referral_controller.uni_comission_count(1);
+  } else if ((uni_days = "montly")) {
+    if (currentDay === 1) {
+      await referral_controller.uni_comission_count(daysBetween);
+    }
+  } else if ((uni_days = "weekly")) {
+    if (currentDayOfWeek === 1) {
+      await referral_controller.uni_comission_count(7);
+    }
+  }
+}
+
+async function getdaysBetween() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentYear = currentDate.getFullYear();
+
+  const previousMonth = currentMonth === 1 ? 12 : currentMonth - 1;
+  const previousYear = currentMonth === 1 ? currentYear - 1 : currentYear;
+
+  const firstDayOfPreviousMonth = new Date(previousYear, previousMonth - 1, 1);
+  const firstDayOfCurrentMonth = new Date(currentYear, currentMonth - 1, 1);
+
+  const daysBetween = Math.round(
+    (firstDayOfCurrentMonth - firstDayOfPreviousMonth) / (1000 * 60 * 60 * 24),
+  );
+  return daysBetween;
+}
+
 module.exports = {
   delete_referral_settings,
   edit_referral_setting,
   get_referral_setting,
+  testunicalc,
+  testbinarycalc,
 };
