ca900957b79dfdf007e0050a89d35805a1026d50
gogaokradze <deathofsuper@gmail.com>
2023-07-31 17:37:05 +0400

    fix

--- controllers/transaction_controller.js
+++ controllers/transaction_controller.js
@@ -1,90 +1,95 @@
-const { options, transactions, accounts } = require("@cubitrix/models");
+const { transactions, treasuries } = require("@cubitrix/models");
 const main_helper = require("../helpers/index");
 var mongoose = require("mongoose");
+
+// const change_transaction_status = async (req, res) => {
+//   try {
+//     let { _id, tx_status } = req.body;
+//     _id = mongoose.Types.ObjectId(_id);
+
+//     let tx = await transactions.findOne({ _id });
+//     if (!tx) {
+//       return main_helper.error_response(res, "transaction not found");
+//     }
+//     if (tx.tx_type == "payment") {
+//       return main_helper.error_response(
+//         res,
+//         "cannot change status to payment transaction",
+//       );
+//     }
+//     const [updateTx, updateTreasury] = await Promise.all([
+//       transactions.findOneAndUpdate({ _id }, { tx_status }),
+//       treasuries.findOneAndUpdate(
+//         {},
+//         {
+//           $inc: {
+//             [`withdrawals.${tx?.tx_options?.currency?.toUpperCase()}`]: tx.amount,
+//           },
+//         },
+//       ),
+//     ]);
+//     if (updateTx) {
+//       return main_helper.success_response(res, updateTx);
+//     } else {
+//       return main_helper.error_response(res, "error");
+//     }
+//   } catch (e) {
+//     console.log(e.message);
+//     return main_helper.error_response(res, "error");
+//   }
+// };
 
 const change_transaction_status = async (req, res) => {
   try {
     let { _id, tx_status } = req.body;
     _id = mongoose.Types.ObjectId(_id);
 
-    let tx = await transactions.findOne({ _id });
+    let [tx, treasury] = await Promise.all([
+      transactions.findOne({ _id }),
+      treasuries.findOne(),
+    ]);
+
     if (!tx) {
       return main_helper.error_response(res, "transaction not found");
     }
     if (tx.tx_type == "payment") {
       return main_helper.error_response(
         res,
-        "cannot change status to payment transaction"
+        "cannot change status to payment transaction",
       );
     }
-    let updateTx = await transactions.findOneAndUpdate({ _id }, { tx_status });
+
+    let promises = [transactions.findOneAndUpdate({ _id }, { tx_status })];
+
+    if (tx.tx_status == "pending" && tx_status == "approved") {
+      const currency = tx?.tx_options?.currency?.toUpperCase();
+      const currentWithdrawalAmount = treasury.withdrawals[currency] || 0;
+      const currentIncomingAmount = treasury.incoming[currency] || 0;
+      console.log(currentWithdrawalAmount, tx.amount, currentIncomingAmount);
+      if (currentWithdrawalAmount + tx.amount > currentIncomingAmount) {
+        return main_helper.error_response(
+          res,
+          "Insufficient funds for withdrawal in treasury",
+        );
+      }
+      promises.push(
+        treasuries.findOneAndUpdate(
+          {},
+          {
+            $inc: {
+              [`withdrawals.${currency}`]: tx.amount,
+            },
+          },
+        ),
+      );
+    }
+
+    const [updateTx, updateTreasury] = await Promise.all(promises);
     if (updateTx) {
       return main_helper.success_response(res, updateTx);
     } else {
       return main_helper.error_response(res, "error");
     }
-    let from_account = await accounts.findOne({
-      account_owner: tx.from,
-      account_category: tx.tx_options.account_category_from,
-    });
-    let to_account = await accounts.findOne({
-      account_owner: tx.to,
-      account_category: tx.tx_options.account_category_to,
-    });
-    console.log(from_account, to_account);
-    let tx_amount = tx.amount;
-    let tx_prev_status = tx.tx_status;
-    if (tx_prev_status == tx_status) {
-      return main_helper.error_response(res, "transaction status is same");
-    }
-
-    // if (tx_prev_status == "pending") {
-    //   if (tx_status == "cancelled") {
-    //     if (tx_amount > to_account.balance) {
-    //       return main_helper.error_response(res, "insufficient balance");
-    //     }
-    //     if (!from_account && !to_account) {
-    //       return main_helper.error_response(res, "incorrect addresses");
-    //     }
-    //     let from_account_change = await accounts.findOneAndUpdate(
-    //       {
-    //         account_owner: tx.from,
-    //         account_category: tx.tx_options.account_category_from,
-    //       },
-    //       { $inc: { balance: tx_amount } }
-    //     );
-    //     let to_account_change = await accounts.findOneAndUpdate(
-    //       {
-    //         account_owner: tx.to,
-    //         account_category: tx.tx_options.account_category_to,
-    //       },
-    //       { $inc: { balance: -tx_amount } }
-    //     );
-    //     if (from_account_change && to_account_change) {
-    //       let updateTx = await transactions.findOneAndUpdate(
-    //         { _id },
-    //         { tx_status }
-    //       );
-    //       if (updateTx) {
-    //         return main_helper.success_response(res, updateTx);
-    //       } else {
-    //         return main_helper.error_response(res, "error");
-    //       }
-    //     }
-    //   } else {
-    //     let updateTx = await transactions.findOneAndUpdate(
-    //       { _id },
-    //       { tx_status }
-    //     );
-    //     if (updateTx) {
-    //       return main_helper.success_response(res, updateTx);
-    //     } else {
-    //       return main_helper.error_response(res, "error");
-    //     }
-    //   }
-    // } else {
-    //   return main_helper.error_response(res, "error");
-    // }
   } catch (e) {
     console.log(e.message);
     return main_helper.error_response(res, "error");
@@ -93,11 +98,10 @@ const change_transaction_status = async (req, res) => {
 
 const edit_transaction = async (req, res) => {
   try {
-    let { _id, amount, from, to, tx_currency, tx_hash, tx_status, tx_type } =
-      req.body;
+    let { _id, amount, from, to, tx_currency, tx_hash, tx_status, tx_type } = req.body;
     let updateTx = await transactions.findOneAndUpdate(
       { _id },
-      { amount, from, to, tx_currency, tx_hash, tx_status, tx_type }
+      { amount, from, to, tx_currency, tx_hash, tx_status, tx_type },
     );
     if (updateTx) {
       return main_helper.success_response(res, updateTx);
