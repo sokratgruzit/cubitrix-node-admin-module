96461609a2a814b771e7deb3652305dde7bead0d
David Tavadze <tavadzed@gmail.com>
2024-03-23 15:46:40 +0400

    last

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -1,7 +1,7 @@
 const allowed_origins = [
-    // 'http://localhost:3000',
-    // 'http://localhost:3001'
-    "https://a1-server.onrender.com"
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://a1-server.onrender.com'
 ];
 
 module.exports = allowed_origins;
